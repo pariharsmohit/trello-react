b11561f50da709ea51a36dd196d58071a4462042
Mohit Singh Parihar <mohitsinghparihar@gmail.com>
2020-08-28 15:49:47 +0530

    import @material -ui/Icon

--- src/components/TrelloActionButton.js
+++ src/components/TrelloActionButton.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Icon from '@material-ui/core/icon';
+import Icon from '@material-ui/core/Icon';
 import Textarea from 'react-textarea-autosize';
 import Card from '@material-ui/core/Card';
 import Button from '@material-ui/core/Button';
